725b18e4a3873f721fba2818b4a3cda6dca54ec3
evgenclash <evgen.clash8991@gmail.com>
2021-04-29 19:06:38 +0300

    pull request added

---
+++ app/operators.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class operators
+{
+//    properties
+    public string $name;
+    public string $operatorUri;
+    public string $cardUri;
+    public string $cardLogoUri;
+//    methods
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    public function set_operator_uri($operatorUri){
+        $this->operatorUri = $operatorUri;
+    }
+
+    public function set_card_uri($cardUri){
+        $this->cardUri = $cardUri;
+    }
+
+    public function set_logo_uri($logoUri){
+        $this->cardLogoUri = $logoUri;
+    }
+
+    public function __get($name){
+
+        return $this->name;
+}
+
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-include_once('functions/functions.reg.php');
+include_once('../functions/functions.reg.php');
+include_once('../app/operators.php');
+
 
 $url = 'https://www.ubisoft.com/en-gb/game/rainbow-six/siege/game-info/operators?fbclid=IwAR0hE7Rjar0iT52mQtp9FaYL5ezVY3I_Th_KnCpH2ExvLlOE0eHKP6s-kTo&isSso=true&refreshStatus=noLoginData';
 
@@ -13,29 +15,30 @@ $html = curl_get($url);
 $html2 = get_html2($url);
 //get the html code of the operators using our parser
 $ops = get_operators($html);
-
-foreach ($ops[0] as $operator){
-//get the operatorsUri
-    $operatorUri = get_operatorUri($operator);
-    $operators[$i]['operatorUri'] = $header. $operatorUri;
-//    get the cardLogoUri
-    $cardLogoUri = get_cardLogoUri($operator);
-    $operators[$i]['cardLogoUri'] = $cardLogoUri;
-//    get the cardUri
-    $cardUri = get_cardUri($operator);
-    $operators[$i]['cardUri'] = $cardUri;
+foreach ($ops[0] as $i => $operator){
     //    get the name
     $name = get_name($operator);
-    $operators[$i]['name'] = $name;
-//increment the counter
-    $i++;
+    $operators['name'] = $name;
+    $name = new operators($name);
+//get the operatorsUri
+    $operatorUri = get_operatorUri($operator);
+    $name->set_operator_uri($header. $operatorUri);
+//    get the cardLogoUri
+    $cardLogoUri = get_cardLogoUri($operator);
+    $name->set_logo_uri($cardLogoUri);
+//    get the cardUri
+    $cardUri = get_cardUri($operator);
+    $name->set_card_uri($cardUri);
+
+    $objectOperators[] = $name;
 }
 
-foreach ($operators as $operator){
-    echo $operator['name']. '<br>';
-    echo $operator['operatorUri']. '<br>';
-    echo $operator['cardUri']. '<br>';
-    echo $operator['cardLogoUri']. '<br>'. '<br>';
+foreach ($objectOperators as $operator){
+    echo $operator->name. '<br>';
+
+    echo $operator->operatorUri. '<br>';
+    echo $operator->cardUri. '<br>';
+    echo $operator->cardLogoUri. '<br>'. '<br>';
 }
 
 
