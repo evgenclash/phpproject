bc02b29d0476143c8694a571d996101b2fbdd5f8
evgenclash <evgen.clash8991@gmail.com>
2021-05-24 23:53:41 +0300

    dump added

--- database/Operators_team.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Operators.team (ID, teamNR, Name, dateTime) VALUES (1, 1, 'Jäger', '2021-05-19 20:02:01');
-INSERT INTO Operators.team (ID, teamNR, Name, dateTime) VALUES (2, 1, 'NØKK', '2021-05-19 20:02:01');
-INSERT INTO Operators.team (ID, teamNR, Name, dateTime) VALUES (3, 1, 'Rook', '2021-05-19 20:02:01');
-INSERT INTO Operators.team (ID, teamNR, Name, dateTime) VALUES (4, 1, 'Lion', '2021-05-19 20:02:01');
-INSERT INTO Operators.team (ID, teamNR, Name, dateTime) VALUES (5, 1, 'Gridlock', '2021-05-19 20:02:01');
